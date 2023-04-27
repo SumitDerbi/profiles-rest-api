91a6efa0b520f6a0a085b071eb51530f6ba18202
Sumit Darbeshwar <s.sumit4us@gmail.com>
2023-04-27 21:00:50 +0530

    Added permissions to feed api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,3 +9,13 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id == request.user.id
+
+
+class UpdateOwnFeed(permissions.BasePermission):
+    """Allow users to update own profile"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user updating update"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
